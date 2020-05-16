c1a604cbc7bb8b735154824fb7bfe781c9be424a
Cristian Ayala <caayala@uc.cl>
2020-05-16 10:18:31 -0400

    base para grafico de arco

---
+++ R/05-arc.R
@@ -0,0 +1,7 @@
+
+# Se requiere abrir la base de datos de la Encuesta Bicentenario 2019. 
+# Disponible en: https://encuestabicentenario.uc.cl
+
+library(tidyverse)
+
+df_bicen_19 <- readRDS('inputs/05-arc-df_bicen_19_30diasdegraficos_2020.rds')
