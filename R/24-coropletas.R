9a80471a41ab33f1ff0929b6f0c43409571d3f7c
Cristian Ayala <caayala@uc.cl>
2020-06-04 14:29:24 -0400

    Modificación de animación
    Agregar leyenda de comunas
    Ajsutar límites del mapa.
    Crear bases de datos con información para reproducir el gráfico.

--- R/24-coropletas.R
+++ R/24-coropletas.R
@@ -2,86 +2,72 @@
 # Paquetes ----------------------------------------------------------------
 
 library(sf)
+library(tidyverse)
+library(gganimate)
 library(desuctools)
 
 # Base de datos -----------------------------------------------------------
 
-df_comunas <- read_sf('inputs/COMUNA_C17.shp') %>% 
-  mutate_at(vars(region, comuna, provincia), as.integer)
-
-nacimientos <- readRDS('inputs/01-df_nacimientos.rds')
-
+# Cartografía comunal del Gran Santiago
+sf_comunas_gs <- readRDS('inputs/24-sf_comunas_gs.rds')
 
 # Embarazo adolescente
 
-
-emb_adolecente <- function(edad){
-  
-  niveles <- c('14 años o menos', '15 o 16 años', '17 o 19 años', 'no EA')
-  
-  case_when(edad %in%  1:14          ~ '14 años o menos',
-            edad %in% 15:16          ~ '15 o 16 años',
-            edad %in% 17:19          ~ '17 o 19 años',
-            is.na(edad) | edad == 99 ~ NA_character_,
-            TRUE                     ~ 'no EA') %>% 
-    as_factor() %>% 
-    fct_relevel(niveles)
-}
-
-nacimientos <- nacimientos %>% 
-  mutate(edad_padre = coalesce(edad_padre, edad_p),
-         edad_madre = coalesce(edad_madre, edad_m),
-         ea_padre = emb_adolecente(edad_padre),
-         ea_madre = emb_adolecente(edad_madre))
-
-nacimientos %>% 
-  skimr::skim(edad_padre, edad_madre, ea_padre, ea_madre)
-
-#eliminar missing
-sum(nacimientos$edad_madre > 60, na.rm = TRUE)
+# Base de datos con información por comuna y año respecto de la cantidad de nacimientos registrados
+# según la edad de la madre `ea_madre`.
+ea_anos_comuna <- readRDS('inputs/24-nacimientos_comunas_anos.rds')
 
 
-
-nacimientos <- nacimientos %>% 
-  filter(edad_madre < 60)
-
-# Gráfico -----------------------------------------------------------------
-
-ea_anos_comuna <- nacimientos %>% 
-  filter(ano_nacim > 2000 & comuna != 0) %>% 
-  count(ano_nacim, ea_madre, comuna) %>% 
-  group_by(comuna, ano_nacim) %>% 
-  mutate(total = sum(n),
-         prop_ea = 1-n/total) %>% 
+# Cantidad de nacimientos por comuna y por años según si son Embarazo Adolecente (EA) o no
+ea_anos_comuna <- ea_anos_comuna %>% 
+  add_count(comuna, ano_nacim, wt = n, name = 'total') %>%
+  mutate(prop_ea = 1 - n/total) %>% 
   filter(ea_madre == 'no EA') %>% 
-  select(-ea_madre) %>% 
-  rename(noEA = n) %>% 
-  arrange(comuna)
+  rename(noEA = n)
 
-comunas_gran_santiago <- regiones_y_comunas %>% 
-  filter(gran_santiago) %>% 
-  pull(comuna)
+# Unir cartografía con datos de embarazo adolescente.
+ea_anos_comuna_gs <- inner_join(ea_anos_comuna,
+                                sf_comunas_gs,
+                                by = 'comuna')
 
-ea_anos_comuna_gs <- ea_anos_comuna %>% 
-  filter(comuna %in% comunas_gran_santiago) %>% 
-  left_join(df_comunas, 
-            by = 'comuna')
+# Transformar data.frame a sf
+ea_anos_comuna_gs <- sf::st_as_sf(ea_anos_comuna_gs)
+
+ea_anos_comuna_gs <- ea_anos_comuna_gs %>% 
+  mutate(nom_comuna = str_wrap(str_to_lower(nom_comuna), width = 8))
+
+st_bbox(ea_anos_comuna_gs)
 
 gg_ea_gran_santiago <- ea_anos_comuna_gs %>% 
-  # filter(ano_nacim == 2017) %>%
   ggplot(aes(fill = prop_ea)) +
   geom_sf() +
+  geom_sf_text(aes(label = nom_comuna), 
+               size = rel(2.5),
+               alpha = .9, colour = 'gray90') + 
+  coord_sf(xlim = c(-70.8, -70.5), 
+           ylim = c(-33.65, -33.32),
+           expand = FALSE) + 
   scale_fill_viridis_c(name = 'Proporción\nembarazo\nadolescente',
+                       direction = -1,
                        labels = function(x) scales::percent(x, accuracy = 2)) +
   labs(subtitle = 'Nacimientos de menores de 19 años partido por nacimientos totales',
-       caption = 'DESUC, a partir de datos INE')
+       caption = 'DESUC, a partir de datos INE') +
+  theme_minimal() + 
+  theme(axis.text = element_blank(),
+        axis.title = element_blank())
+
+gg_ea_gran_santiago
 
 gg_ea_gran_santiago_anim <- gg_ea_gran_santiago +
   transition_states(ano_nacim,
                     transition_length = 1,
                     state_length = 3) +
-  labs(title = 'Proporción de embarazo adolescente según año. \nAño {closest_state}')
+  labs(title = 'Proporción de embarazo adolescente según año. \nAño {closest_state} Gran Santiago')
 
 animate(
-  gg_ea_gran_santiago_anim
+  gg_ea_gran_santiago_anim,
+  start_pause = 2,
+  end_pause = 10
 )
+
+anim_save('outputs/24-coropletas.mp4')
