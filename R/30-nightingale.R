3b01d51abc7254411e1d2f3139291590a43fb4c6
Denisse Devilat <dbdevila@uc.cl>
2020-06-10 11:13:18 -0400

    Update 30-nightingale.R
    s

--- R/30-nightingale.R
+++ R/30-nightingale.R
@@ -26,7 +26,7 @@ base <- base %>%
                            v13 == 2 ~ 2,
                            v13 == 4 ~ 2,
                            v13 == 5 ~ 3,
-                           v13 == 6 ~ 4,
+                           v13 == 6 ~ 3,
                            v13 == 7 ~ 5,
                            v13 == 8 ~ 5,
                            v13 == 9 ~ 6,
@@ -36,8 +36,8 @@ base <- base %>%
          v13_r = haven::labelled(v13_r,
                                 labels = c('Propia o compartida \npagada' = 1,
                                            'Propia o compartida \npagándose' = 2,
-                                           'Arrendada con contrato' = 3,
-                                           'Arrendada sin contrato' = 4,
+                                           'Arrendada' = 3,
+                                           # 'Arrendada sin contrato' = 4,
                                            'Cedida \n(por trabajo o por familiar)' = 5,
                                            'Usufructo/\nOcupación o Posesión irregular' = 6),
                                 label = 'Situación bajo la cual su hogar ocupa la vivienda'))
@@ -45,26 +45,30 @@ base <- base %>%
 # Gráfico -------------------------------------------------------
 
 tab <- base %>% 
-  tabla_vars_segmentos(.vars = vars(dau),
-                       .segmentos = vars(v13_r),
+  tabla_vars_segmentos(.vars = vars(v13_r),
+                       .segmentos = vars(dau),
                        miss = NA,
                        .wt = expr) %>% 
   filter(segmento_cat != 'No sabe/no responde',
          !is.na(segmento_cat),
          !is.na(pregunta_cat))
 
-gg_polar <- ggplot(tab, aes(x = pregunta_cat, y = prop,  fill = as.factor(segmento_cat))) + 
-  geom_bar(stat = "identity", position = "identity", width = 1) +
-  coord_polar() +
+gg_polar <- ggplot(tab, aes(x = segmento_cat, y = prop,  fill = as.factor(pregunta_cat))) + 
+  geom_bar(stat = "identity", position = "identity", width = 1, color = "gray20") +
+  coord_polar(start = 3 * pi / 2) +
   # geom_text(aes(label = scales::percent(prop)), position = position_stack(vjust = 0.5)) +
+  # scale_y_continuous('', expand = c(0, 0), 
+  #                    limits = c(0,1), 
+  #                    labels = function(x) scales::percent(x, accuracy = 1)) +
   scale_fill_brewer(palette = "Spectral") +
   theme_minimal() +
   theme(plot.title = element_text(size = rel(1.1)),
         axis.title.x = element_blank(),
         axis.title.y = element_blank(),
-        axis.text.y = element_blank(),
+        # axis.text.y = element_blank(),
         legend.title = element_blank()) +
   labs(title = 'Situación bajo la cual su hogar ocupa la vivienda, según decil',
        caption = "Casen 2017")
 
 gg_polar
+
